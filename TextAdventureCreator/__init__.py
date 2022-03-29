f2cbbb26b8201543284879d029b90949b9a6f795
AlbertUnruh <AlbertUnruh@pm.me>
2022-03-29 17:49:26 +0200

    Initial commit

---
+++ TextAdventureCreator/__init__.py
@@ -0,0 +1,6 @@
+__author__ = "AlbertUnruh"
+__version__ = "0.0.0a"
+__license__ = "GNU AGPLv3"
+__copyright__ = f"Copyright 2022-present (c) {__author__}"
+__url__ = "https://github.com/AlbertUnruh/TextAdventureCreator"
+__description__ = "A simple map creator for [TextAdventure](https://github.com/AlbertUnruh/TextAdventure)"
